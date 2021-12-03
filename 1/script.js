bd0b5028947e69012f956b83bde2357708e3b01f
Denis Sleptsov <dinis68@mail.ru>
2021-12-04 00:36:18 +0300

    1

---
+++ 1/script.js
@@ -0,0 +1,17 @@
+const btn = document.querySelector('.res-btn');
+const input = document.querySelector ('.txt-input');
+const input = document.querySelector ('.txt-input-b');
+const input = document.querySelector ('.txt-input-c');
+
+btn.addEventListener('click', function (event) {
+
+let a=
+let b=
+let c=
+
+let d= Math.pow(b) - 4*a*c;
+
+let x1 = -b + Math.sqrt(d) / 2;
+let x2 = -b - Math.sqrt(d) / 2;
+}
+);
