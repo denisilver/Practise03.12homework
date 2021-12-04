ae39aa4306177e66b782b4169e2c28288b416640
Denis Sleptsov <dinis68@mail.ru>
2021-12-04 18:30:54 +0300

    1

--- 1/script.js
+++ 1/script.js
@@ -1,17 +1,33 @@
 const btn = document.querySelector('.res-btn');
-const input = document.querySelector ('.txt-input');
-const input = document.querySelector ('.txt-input-b');
-const input = document.querySelector ('.txt-input-c');
+const input1 = document.querySelector ('a');
+const input2 = document.querySelector ('b');
+const input3 = document.querySelector ('c');
 
-btn.addEventListener('click', function (event) {
+btn.addEventListener('click', function (event) 
+{
 
-let a=
-let b=
-let c=
+let a=input1.value;
+let b=input2.value;
+let c=input3.value;
+let d= Math.pow(b) - (4*a*c);
+alert(d);
 
-let d= Math.pow(b) - 4*a*c;
-
-let x1 = -b + Math.sqrt(d) / 2;
-let x2 = -b - Math.sqrt(d) / 2;
+if (d < 0)
+{
+    alert("Неопределен")
 }
-);
+
+if (d > 0) 
+{
+
+let x1 = -b + Math.sqrt(d) / 2 * a;
+let x2 = -b - Math.sqrt(d) / 2 * a;
+alert('x1 = ' + x1);
+alert('x2 = ' + x2);
+}
+if (d == 0) {
+    let x = (-b) / 2 * a;
+    alert('x = ' + x);
+}
+
+
